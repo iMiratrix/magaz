265469f8a115fb365d945a01d06458a5166a2270
iMiratrix <32966401+iMiratrix@users.noreply.github.com>
2019-12-16 11:05:02 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Magaz</title>
+</head>
+<body>
+
+<header>
+
+    <div class="wrap-logo">
+        <a href="" class="logo">Magaz</a>
+    </div>
+    <nav>
+        <a class="" href="">Главная</a>
+
+        <a href="signUp.php">Регистрация</a>
+        <a href="">Корзина</a>
+    </nav>
+
+</header>
+
+<main>
+
+    <div class="container content">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="list-group">
+                    <a href="#" class="list-group-item">Популярное</a>
+                    <a href="#" class="list-group-item">Новое</a>
+                    <a href="#" class="list-group-item">Бестселлеры</a>
+                    <a href="#" class="list-group-item">Классика</a>
+                    <a href="#" class="list-group-item">Подарки на НГ</a>
+                </div>
+            </div>
+
+            <div class="col-md-8 products">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="img/1017285601.jpg" alt=""></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Assassin's Creed. Закатное солнце</a>
+                            </p>
+                            <p class="product-desc">Энтони дель Кол, Конор МакКрири</p>
+                            <p class="product-price">Цена: 1000₽</p>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="img/6002571321.jpg" alt=""></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Diablo. Книга Каина</a>
+                            </p>
+                            <p class="product-desc">Флинт Дилл</p>
+                            <p class="product-price">Цена: 1 800 ₽</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="img/1017285601.jpg" alt=""></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Assassin's Creed. Закатное солнце</a>
+                            </p>
+                            <p class="product-desc">Энтони дель Кол, Конор МакКрири</p>
+                            <p class="product-price">Цена: 1000₽</p>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="img/6002571321.jpg" alt=""></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Diablo. Книга Каина</a>
+                            </p>
+                            <p class="product-desc">Флинт Дилл</p>
+                            <p class="product-price">Цена: 1 800 ₽</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<footer>
+<a>magaz</a>
+</footer>
+
+</body>
+</html>
+<?php
